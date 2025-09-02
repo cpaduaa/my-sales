60aea8296dc78f883c42c9d71bdc3a0da2206995
c.paduaa <cris.padua.96@gmail.com>
2025-09-02 19:04:30 +0100

    feat: authMiddleware

--- src/modules/users/routes/UserRoutes.ts
+++ src/modules/users/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersControllers';
 import { createUserSchema } from '../schemas/UserSchemas';
+import AuthMiddleware from '@shared/middlewares/authMiddleware';
 
 const userRouter = Router();
 const usersController = new UsersController();
 
-userRouter.get('/', usersController.list);
+userRouter.get('/', AuthMiddleware.excute, usersController.list);
 userRouter.post('/', createUserSchema, usersController.create);
 
 export default userRouter;
---
+++ src/shared/middlewares/authMiddleware.ts
@@ -0,0 +1,35 @@
+import AppError from "@shared/errors/AppError";
+import { NextFunction, Response, Request } from "express";
+import { Secret, verify } from "jsonwebtoken";
+
+interface ITokenPayLoad {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default class AuthMiddleware {
+  static excute(request: Request, response: Response, next: NextFunction): void {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError('JWT token is missing', 401);
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+      const decodedToken = verify(token, process.env.APP_SECRET as Secret);
+
+      const { sub } = decodedToken as ITokenPayLoad;
+
+      request.user = {
+        id: sub
+      }
+
+      return next();
+    } catch (error){
+      throw new AppError('Invalid JWT token', 401);
+    }
+  }
+}
